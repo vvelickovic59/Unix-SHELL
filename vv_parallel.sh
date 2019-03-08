7f0ee1397aa4e1d74f1ff443ce2512d577c9ed23
vvelickovic59 <48203281+vvelickovic59@users.noreply.github.com>
2019-03-08 15:02:35 +0100

    Update vv_parallel.sh
    Support for multiple arguments with context {} replace

--- vv_parallel.sh
+++ vv_parallel.sh
@@ -15,6 +15,50 @@ declare -l process_log
 
 trap vv_exit INT TERM
 
+function wait_for_task
+{
+   esc_out="\>"; esc_in="\<"; esc_pipe="\|"
+   loc_child=$1
+   shift
+   loc_parameters=$@
+   loc_parameters="${loc_parameters//\[\]/$loc_child}"
+   loc_parameters="${loc_parameters//\|/ \|}"
+   loc_parameters="${loc_parameters//\>/ \>}"
+   loc_parameters="${loc_parameters//;/ ;}"
+
+   ###################### REPLACE STDIN WITH CHILD FIFO ##################################
+   exec 0< $fifo_child
+
+   while :; do
+      ################### LOCK THE PIPE AND WAIT FOR THE TASK ############################
+      flock 0
+
+      IFS= read line && {   flock -u 0
+                            #[ $to_log -gt 0 ] && echo child $parentPID $line >> $process_log
+                            [ "$line" = 'quit' ] && {   echo child_quit > $fifo_parent
+                                                        [ $to_log -gt 0 ] && echo child QUIT $parentPID >> $process_log
+                                                        break
+                                                    }
+
+                            ################### EXECUTE THE TASK #########################
+#                            sleep 1
+                            loc_command=()
+                            for i in ${loc_parameters[@]}; do
+                               [ -z "${i##*\{*}" ] && {   for j in ${line}; do
+                                                             loc_command+=( "${i//\{\}/$j}" )
+                                                          done
+                                                      } || loc_command+=( $i )
+                            done
+
+                            eval ${loc_command[@]}
+                            echo $? > $fifo_parent
+                        }
+   done
+
+   ###################### RESET FD #######################################################
+   exec 0<&-
+}
+
 function vv_exit
 {
    i=1
@@ -37,38 +81,6 @@ function vv_exit
    exit
 }
 
-function wait_for_task
-{
-   loc_child=$1
-   shift
-
-   ###################### REPLACE STDIN WITH CHILD FIFO ##################################
-   exec 0< $fifo_child
-
-   while :; do
-      ################### LOCK THE PIPE AND WAIT FOR THE TASK ############################
-      flock 0
-
-      IFS= read line && {   flock -u 0
-                            #[ $to_log -gt 0 ] && echo child $parentPID $line >> $process_log
-                            [ "$line" = 'quit' ] && {   echo child_quit > $fifo_parent
-                                                        [ $to_log -gt 0 ] && echo child QUIT $parentPID >> $process_log
-                                                        break
-                                                    }
-
-                            ################### EXECUTE THE TASK #########################
-#                            sleep 1
-                            loc_command="${@//\{\}/$line}"
-                            loc_command="${loc_command[@]//\[\]/$loc_child}"
-                            eval ${loc_command[@]}
-                            echo $? > $fifo_parent
-                        }
-   done
-
-   ###################### RESET FD #######################################################
-   exec 0<&-
-}
-
 function usage
 {
    clear
@@ -79,6 +91,8 @@ function usage
    echo "USAGE: $0 parallel [-p x -l] [command command_parameters...]"
    echo " "
    echo "   - parallel                     - (MANDATORY) Number of parallel processes"
+   echo "   - -X x                         - (OPTIONAL) Multiple (i.e. x>=1) arguments with context replace. If {} is used multiple times each {} will be replaced with the arguments."
+   echo "                                    If {} is used as part of a word then the whole word will be repeated."
    echo "   - -p x                         - (OPTIONAL) print progress to STDERR using modulo x (x>=1)"
    echo "   - -l                           - (OPTIONAL) set process log ON"
    echo "   - command                      - (OPTIONAL) command or script to be executed"
@@ -103,14 +117,19 @@ fifo_parent="/tmp/vv_parent_fifo_$parentPID"
 process_log="/tmp/vv_process_log_$parentPID"
 
 ######################### PARSE PARAMETERS ###############################################
-parallel=0; progress=0; progress_bar='.'; to_log=0
+parallel=0; progress=0; progress_bar='.'; to_log=0; loc_xargs=1
 while :; do
+   [[ "$1" =~ "-X" ]] && {   shift
+                             [ -z "${1##*[!0-9]*}" ] && usage || loc_xargs=$1
+                             shift
+                             continue
+                         }
    [[ "$1" =~ "-p" ]] && {   [ ! -z "${1##-p}" ] && progress_bar="${1##-p}"
-                          shift
-                          [ -z "${1##*[!0-9]*}" ] && usage || progress=$1
-                          shift
-                          continue
-                      }
+                             shift
+                             [ -z "${1##*[!0-9]*}" ] && usage || progress=$1
+                             shift
+                             continue
+                         }
    [ "$1" == "-l" ] && {   to_log=1
                            shift
                            continue
@@ -124,6 +143,8 @@ while :; do
 done
 
 [ $parallel -gt 0 ] || usage
+
+[ $loc_xargs -gt 0 ] || loc_xargs=$parallel
 
 [ $to_log -gt 0 ] && {   echo PARALLEL: $parallel >> $process_log
                          [ $progress -gt 0 ] && echo PROGRESS: $progress BAR: $progress_bar >> $process_log || echo NO PROGRESS >> $process_log
@@ -145,8 +166,14 @@ child_number=$((i-1))
 
 exec 4> $fifo_child
 
-i=1; j=0
+i=1; j=0; loc_seq_arr=(); loc_xargs_count=1
 while IFS= read loc_seq; do
+
+   ###################### SUPPORT FOR MULTIPLE ARGUMENTS #################################
+   loc_seq_arr+=( $loc_seq )
+   ((loc_xargs_count++))
+   [ $loc_xargs_count -le $loc_xargs ] && continue
+
    [ $i -gt $parallel ] && {   while :; do
                                   ################### READ PARENT PIPE ###################
                                   IFS= read -u 3 line
@@ -177,11 +204,16 @@ while IFS= read loc_seq; do
    [ "$line" = 'quit' ] && break
 
    ###################### DISTRIBUTE TASKS TO CHILD PROCESSES ############################
-   echo "$loc_seq" > $fifo_child
-   ((i++)); ((j++))
-#   [ $progress -gt 1 ] && [ $(( $j % $progress )) -eq 0 ] && printf $j || [ $progress -gt 0 ] && printf $progress_bar
+   echo ${loc_seq_arr[@]} > $fifo_child
+   loc_seq_arr=(); loc_xargs_count=1; ((i++)); ((j++))
+
+   ###################### PRINT PROGRESS #################################################
    [ $progress -gt 1 ] && [ $(( $j % $progress )) -eq 0 ] && printf $j>&2 || [ $progress -gt 0 ] && printf $progress_bar>&2
 done
+
+######################### DISTRIBUTE REMAINING TASKS TO CHILD PROCESSES ##################
+[ ${#loc_seq_arr[@]} -eq 0 ] || echo ${loc_seq_arr[@]} > $fifo_child
+
 [ $progress -gt 0 ] && printf "%s\n" $j>&2
 
 vv_exit
